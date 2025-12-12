fea26f7c123769a8f86a702c1f938d371b8207dd
Elisey Boyko <116101705+NiGrammaMorali@users.noreply.github.com>
2025-12-12 16:32:17 +0300

    Implemented constants and functions

--- src/calc.cpp
+++ src/calc.cpp
@@ -5,12 +5,72 @@ ArithmExpression::ArithmExpression(string inf) : infix(inf) {
 	ToPostfix();
 }
 void ArithmExpression::Parse() {
+	string lexem;
+	bool flag_l = false;
+	bool flag_n = false;
 	for (char c : infix) {
-		lexems.push_back(c);
+		if (c == '\n') {
+			break;
+		}
+		if (c >= '0' && c <= '9') {
+			if (flag_l) {
+				lexems.push_back(lexem);
+				lexem.clear();
+				flag_l = false;
+			}
+			lexem.push_back(c);
+			flag_n = true;
+		}
+		else {
+			if (flag_n) {
+				lexems.push_back(lexem);
+				lexem.clear();
+				flag_n = false;
+			}
+			if ((c >= '(' && c <= '+') || c == '-' || c == '/' || c == '^') {
+				if (flag_l) {
+					lexems.push_back(lexem);
+					lexem.clear();
+					flag_l = false;
+				}
+				lexem = c;
+				lexems.push_back(lexem);
+				lexem.clear();
+			}
+			else {
+				flag_l = true;
+				lexem.push_back(c);
+				if (lexem == "sin") {
+					lexems.push_back(lexem);
+					lexem.clear();
+					flag_l = false;
+					continue;
+				}
+				if (lexem == "cos") {
+					lexems.push_back(lexem);
+					lexem.clear();
+					flag_l = false;
+					continue;
+				}
+				if (lexem == "ln") {
+					lexems.push_back(lexem);
+					lexem.clear();
+					flag_l = false;
+					continue;
+				}
+			}
+		}
+	}
+	if (lexem.size() != 0) {
+		lexems.push_back(lexem);
 	}
 }
 int ArithmExpression::GetPriority(char item) {
 	switch (item) {
+	case('"'):
+	case('#'):
+	case('$'):
+		return 3;
 	case('+'):
 	case('-'):
 		return 1;
@@ -26,7 +86,25 @@ void ArithmExpression::ToPostfix() {
 	Parse();
 	DynamicStack<char> Stack;
 	char StackItem;
-	for (char item : lexems) {
+	string tmp;
+	for (string lexem : lexems) {
+		char item;
+		if (lexem == "sin") {
+			lexem = '"';
+		}
+		else if (lexem == "cos") {
+			lexem = '#';
+		}
+		else if (lexem == "ln") {
+			lexem = '$';
+		}
+		if (lexem.size() != 1) {
+			postfix.push_back(lexem);
+			continue;
+		}
+		else {
+			item = lexem[0];
+		}
 		switch (item) {
 		case('('):
 			Stack.Push(item);
@@ -35,8 +113,15 @@ void ArithmExpression::ToPostfix() {
 			StackItem = Stack.Top();
 			Stack.Pop();
 			while (StackItem != '(') {
-				postfix += StackItem;
+				tmp = StackItem;
+				postfix.push_back(tmp);
 				StackItem = Stack.Top();
+				Stack.Pop();
+			}
+			StackItem = Stack.Top();
+			if (StackItem >= '"' && StackItem <= '$') {
+				tmp = StackItem;
+				postfix.push_back(tmp);
 				Stack.Pop();
 			}
 			break;
@@ -44,11 +129,15 @@ void ArithmExpression::ToPostfix() {
 		case('-'):
 		case('*'):
 		case('/'):
+		case('"'):
+		case('#'):
+		case('$'):
 			while (!Stack.IsEmpty()) {
 				StackItem = Stack.Top();
 				Stack.Pop();
 				if (GetPriority(item) <= GetPriority(StackItem)) {
-					postfix += StackItem;
+					tmp = StackItem;
+					postfix.push_back(tmp);
 				}
 				else {
 					Stack.Push(StackItem);
@@ -58,21 +147,26 @@ void ArithmExpression::ToPostfix() {
 			Stack.Push(item);
 			break;
 		default:
-			operands.insert({item, 0.0});
-			postfix += item;
+			operands.insert({ item, 0.0 });
+			postfix.push_back(lexem);
 		}
+		
 	}
 	while (!Stack.IsEmpty()) {
 		StackItem = Stack.Top();
 		Stack.Pop();
-		postfix += StackItem;
+		tmp = StackItem;
+		postfix.push_back(tmp);
 	}
 }
 string ArithmExpression::GetInfix() {
 	return infix;
 }
-string ArithmExpression::GetPostfix() {
-	return postfix;
+void ArithmExpression::GetPostfix() {
+	for (string s : postfix) {
+		cout << s;
+	}
+	cout << '\n';
 }
 vector<char> ArithmExpression::GetOperands() const {
 	vector<char> result;
@@ -90,39 +184,66 @@ double ArithmExpression::Calculate(const map<char, double>& values) {
 	}
 	DynamicStack<double> Stack;
 	double LeftOperand, RightOperand;
-	for (char lexem : postfix) {
-		switch (lexem) {
-		case('+'):
-			RightOperand = Stack.Top();
-			Stack.Pop();
-			LeftOperand = Stack.Top();
-			Stack.Pop();
-			Stack.Push(LeftOperand + RightOperand);
-			break;
-		case('-'):
-			RightOperand = Stack.Top();
-			Stack.Pop();
-			LeftOperand = Stack.Top();
-			Stack.Pop();
-			Stack.Push(LeftOperand - RightOperand);
-			break;
-		case('*'):
-			RightOperand = Stack.Top();
-			Stack.Pop();
-			LeftOperand = Stack.Top();
-			Stack.Pop();
-			Stack.Push(LeftOperand * RightOperand);
-			break;
-		case('/'):
-			RightOperand = Stack.Top();
-			Stack.Pop();
-			LeftOperand = Stack.Top();
-			Stack.Pop();
-			Stack.Push(LeftOperand / RightOperand);
-			break;
-		default:
-			Stack.Push(operands[lexem]);
-			break;
+	for (string lexem : postfix) {
+		char item;
+		if (lexem.size() > 1) {
+			double tmp = stod(lexem);
+			Stack.Push(tmp);
+			cout << Stack.Top() << '\n';
+		}
+		else {
+			item = lexem[0];
+			switch (item) {
+			case('+'):
+				RightOperand = Stack.Top();
+				Stack.Pop();
+				LeftOperand = Stack.Top();
+				Stack.Pop();
+				Stack.Push(LeftOperand + RightOperand);
+				break;
+			case('-'):
+				RightOperand = Stack.Top();
+				Stack.Pop();
+				LeftOperand = Stack.Top();
+				Stack.Pop();
+				Stack.Push(LeftOperand - RightOperand);
+				break;
+			case('*'):
+				RightOperand = Stack.Top();
+				Stack.Pop();
+				LeftOperand = Stack.Top();
+				Stack.Pop();
+				Stack.Push(LeftOperand * RightOperand);
+				break;
+			case('/'):
+				RightOperand = Stack.Top();
+				Stack.Pop();
+				LeftOperand = Stack.Top();
+				Stack.Pop();
+				Stack.Push(LeftOperand / RightOperand);
+				break;
+			case('"'):
+				LeftOperand = Stack.Top();
+				Stack.Pop();
+				LeftOperand = sin(LeftOperand);
+				Stack.Push(LeftOperand);
+				break;
+			case('#'):
+				LeftOperand = Stack.Top();
+				Stack.Pop();
+				LeftOperand = cos(LeftOperand);
+				Stack.Push(LeftOperand);
+				break;
+			case('$'):
+				LeftOperand = Stack.Top();
+				Stack.Pop();
+				LeftOperand = log2(LeftOperand);
+				Stack.Push(LeftOperand);
+				break;
+			default:
+				Stack.Push(operands[item]);
+				break;
+			}
 		}
 	}
 	return Stack.Top();
